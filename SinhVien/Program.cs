3f9634bcfb3d5705ab78a93f868f3f7f33d33595
GiaHao810 <90594281+GiaHao810@users.noreply.github.com>
2022-08-12 07:55:27 +0700

    Update code

--- SinhVien/Program.cs
+++ SinhVien/Program.cs
@@ -13,35 +13,13 @@ namespace SinhVien
         /// The main entry point for the application.
         /// </summary>
 
-        public static Thread dangnhap;
-        public static Thread dangki;
-        public static Thread employee;
         [STAThread]
+        
         static void Main()
-        {
-            dangnhap = new Thread(new ThreadStart(openSignIn));
-            dangki = new Thread(new ThreadStart(openSignUp));
-            employee = new Thread(new ThreadStart(Employee));
-            employee.Start();
-        }
-
-        static void openSignIn()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SignIn_Screen());
-        }
-        static void openSignUp()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SignUp_Screen());
-        }
-        static void Employee()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Employee_Screen());
         }
 
     }
